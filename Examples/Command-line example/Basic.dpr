a78f68dd0022b274f02aca5e27ac1444f7a0dedc
CWBudde <Christian@savioursofsoul.de>
2015-06-08 15:37:43 +0200

    * slightly improved example

--- Examples/Command-line example/Basic.dpr
+++ Examples/Command-line example/Basic.dpr
@@ -8,9 +8,20 @@ uses
   clFFT in '..\..\Source\clFFT.pas',
   System.SysUtils;
 
+var
+  SetupData: TClFftSetupData;
 begin
   try
-    { TODO -oUser -cConsole Main : Code hier einfügen }
+    // initialize setup data
+    ClFftInitSetupData(SetupData);
+
+    // now setup library
+    ClFftSetup(SetupData);
+
+    // now tear down library
+    ClFftTeardown;
+
+    ReadLn;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
